a4500b5e0aa978d81b93642eda5d9bec068435f2
Eric McKee <lyolayale19@gmail.com>
2022-11-12 14:47:06 -0600

    Created async/await & higher order fn to display specific repo info in DOM

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@
 const overview = document.querySelector(".overview");
 const username = "lyolayale";
 const repoList = document.querySelector(".repo-list");
+const repos = document.querySelector(".repos");
+const repoData = document.querySelector(".repo-data");
 
 // ===================
 // ==== async fn ====
@@ -72,6 +74,57 @@ const fetchRepoInfo = function (repos) {
     li.innerHTML = `<h3>${repo.name}</h3>`;
     repoList.append(li);
   }
+};
+
+// ========================
+// ==== event listener ====
+// ========================
+
+// --- click on repo name ---
+repoList.addEventListener("click", function (e) {
+  if (e.target.matches("h3")) {
+    const repoName = e.target.innerText;
+    fetchSpecificRepoInfo(repoName);
+  }
+});
+
+// --- async fn for repo name event listener ---
+const fetchSpecificRepoInfo = async function (repoName) {
+  const res = await fetch(
+    `https://api.github.com/repos/${username}/${repoName}`
+  );
+  const repoInfo = await res.json();
+  const fetchLanguages = await fetch(repoInfo.languages_url);
+  const languageData = await fetchLanguages.json();
+  const languages = [];
+  for (let lang in languageData) {
+    languages.push(lang);
+  }
+
+  displaySpecificRepoInfo(repoInfo, languages);
+  console.log(repoInfo);
+  console.log(languages);
+};
+
+// --- fn for specific repo info event listener ---
+const displaySpecificRepoInfo = function (repoInfo, lang) {
+  repoData.innerHTML = "";
+
+  const repoDiv = document.createElement("div");
+  repoDiv.innerHTML = `
+    <h3>Name: ${repoInfo.name}</h3>
+    <p>Description: ${repoInfo.description}</p>
+    <p>Default Branch: ${repoInfo.default_branch}</p>
+    <p>Languages: ${lang.join(", ")}</p>
+    <a class="visit" href=${
+      repoInfo.html_url
+    } target="_blank" rel="noreferrer noopener">View Repo on GitHub!</a>
+  `;
+
+  repoData.append(repoDiv);
+
+  repoData.classList.remove("hide");
+  repos.classList.add("hide");
 };
 
 // ==== fetch data ====
